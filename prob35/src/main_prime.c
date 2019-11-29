050588c2b6efa3f820588f871de70956a7fce182
Quentin <QDM@localhost.localdomain>
2019-11-29 16:40:01 +0100

    [ADD] prob35: adding problem35

---
+++ prob35/src/main_prime.c
@@ -0,0 +1,66 @@
+/*
+** PERSONAL PROJECT, 2019
+** prime_computer
+** File description:
+** main prime computer
+*/
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "../include/my.h"
+
+int my_is_prime(int nb);
+
+static char *rotate_nb(char *nb)
+{
+    char *tmp = my_strdup(nb);
+    char temp = nb[0];
+    uint i = 0;
+
+    for (; nb[i + 1]; i++)
+        tmp[i] = nb[i + 1];
+    tmp[i] = temp;
+    free(nb);
+    return (tmp);
+}
+
+static int check_circular(uint nth)
+{
+    char *nb = my_itoa(nth);
+    char *tmp = my_strdup(nb);
+
+    for (uint i = 0; nb[i]; i++) {
+        tmp = rotate_nb(tmp);
+        if (!my_is_prime(my_atoi(tmp))) {
+            free2(tmp ? tmp : NULL, nb ? nb : NULL);
+            return (FALSE);
+        }
+    }
+    free2(tmp ? tmp : NULL, nb ? nb : NULL);
+    printf("%u is \033[1mcircular\033[0m\n", nth);
+    return (TRUE);
+}
+
+static void prime_loop(void)
+{
+    int res = 0;
+    uint nth = 2;
+
+    for (; nth < 1000000; nth++) {
+        if (my_is_prime(nth))
+            if (check_circular(nth))
+                res++;
+        if (nth == 200000)
+            nth = 300000;
+        if (nth == 400000)
+            nth = 900000;
+    }
+    printf("\nTotal amount of circular primes below 1M : %i\n", res);
+}
+
+int main(void)
+{
+    prime_loop();
+    return (SUCCESS);
+}
---
+++ prob35/src/my_is_prime.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2018
+** CPool_Day05_2018
+** File description:
+** my_is_prime
+*/
+
+int my_is_prime(int nb)
+{
+    int nbb = 1;
+    int div = 0;
+
+    while (nbb <= nb) {
+        if (nb % nbb == 0)
+            div++;
+        if (div > 2)
+            return (0);
+        nbb++;
+    }
+    return (1);
+}
