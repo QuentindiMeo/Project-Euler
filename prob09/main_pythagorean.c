5bc3e9e9059f8a6a9d39120459aeadc969c7c180
Quentin <QDM@localhost.localdomain>
2019-12-03 14:38:46 +0100

    [ADD] prob09: adding problem9

---
+++ prob09/main_pythagorean.c
@@ -0,0 +1,23 @@
+/*
+** PERSONAL PROJECT, 2019
+** pythagorean
+** File description:
+** main pythagorean
+*/
+
+#include <stdio.h>
+#include "my.h"
+
+int main(void)
+{
+    for (uint a = 1; a < 999; a++)
+        for (uint b = 2; b < 999; b++)
+            for (uint c = 3; c < 999; c++) {
+                if (a * a + b * b == c * c && a + b + c == 1000) {
+                    printf("Triplet: %u, %u, %u\nResult : %u\n",
+                           a, b, c, a * b * c);
+                    return (SUCCESS);
+                }
+            } 
+    return (SUCCESS);
+}
