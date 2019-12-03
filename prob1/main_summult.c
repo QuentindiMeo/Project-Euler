838e15a9ab9790fa1c3e2aa5015bd16ba3f997d4
Quentin <QDM@localhost.localdomain>
2019-12-03 09:34:28 +0100

    [ADD] prob1: adding problem1

---
+++ prob1/main_summult.c
@@ -0,0 +1,36 @@
+/*
+** PERSONAL PROJECT, 2019
+** summult
+** File description:
+** main summult
+*/
+
+#include <stdio.h>
+#include "my.h"
+
+static void summult_loop(uint limit)
+{
+    int res = 0;
+
+    for (uint i = 0; i < limit; i++)
+        if (i % 3 == 0 || i % 5 == 0)
+            res += i;
+    printf("S%d(3, 5) = %i\n", limit, res);
+}
+
+int main(int ac, char **av)
+{
+    if (ac != 2) {
+        printf("USAGE\n\t./summult [limit]\n");
+        return (FAILURE);
+    }
+    if (my_atoi(av[1]) < 0) {
+        printf("USAGE\n\t./summult [limit]\n");
+        return (FAILURE);
+    }
+    if (my_getnbr(av[1]) == 0)
+        printf("S0(3, 5) = 1\n");
+    else
+        summult_loop(my_getnbr(av[1]));
+    return (SUCCESS);
+}
