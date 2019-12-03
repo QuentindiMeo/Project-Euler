53235477a69bee201048f62fbacd8727227ad463
Quentin <QDM@localhost.localdomain>
2019-12-03 13:20:57 +0100

    [ADD] prob05: adding problem5

---
+++ prob05/main_smallest.c
@@ -0,0 +1,29 @@
+/*
+** PERSONAL PROJECT, 2019
+** summult
+** File description:
+** main summult
+*/
+
+#include <stdio.h>
+#include "my.h"
+
+static int mod_is_zero(int n)
+{
+    if (n % 20 == 0 && n % 19 == 0 && n % 18 == 0 && n % 17 == 0 && n % 16 == 0
+        && n % 15 == 0 && n % 14 == 0 && n % 13 == 0 && n % 12 == 0 &&
+        n % 11 == 0 && n % 10 == 0)
+        return (TRUE);
+    return (FALSE);
+}
+
+int main(void)
+{
+    uint i = 20;
+
+    for (; 1; i += 10)
+        if (mod_is_zero(i))
+            break;
+    printf("Smallest multiple: %i\n", i);
+    return (SUCCESS);
+}
