f802df684ea4782e7932569d25a222b954e2df48
Quentin <QDM@localhost.localdomain>
2019-12-05 12:08:24 +0100

    [ADD] prob21: adding problem21

---
+++ prob21/main_amicable.c
@@ -0,0 +1,46 @@
+/*
+** PERSONAL PROJECT, 2019
+** amicable
+** File description:
+** main amicable
+*/
+
+#include <stdio.h>
+#include "my.h"
+
+static int get_sum_prop_div(int nb)
+{
+    int res = 0;
+
+    for (int i = 1; i < nb; i++)
+        if (nb % i == 0)
+            res += i;
+    return (res);
+}
+
+static void amicable(int limit)
+{
+    int res = 0;
+
+    for (int nb = 1; nb < limit; nb++)
+        if (nb == get_sum_prop_div(get_sum_prop_div(nb)))
+            if (nb != get_sum_prop_div(nb)) {
+                printf("works for %d (%d)\n", nb, get_sum_prop_div(nb));
+                res += nb;
+            }
+    printf("S(A(n)) for n < %d: %i\n", limit, res);
+}
+
+int main(int ac, char **av)
+{
+    if (ac != 2) {
+        printf("USAGE\n\t%s [0 < limit < 10000]\n", av[0]);
+        return (FAILURE);
+    }
+    if (my_atoi(av[1]) < 0 || my_atoi(av[1]) >= 100000) {
+        printf("USAGE\n\t%s [0 < limit < 10000]\n", av[0]);
+        return (FAILURE);
+    }
+    amicable(my_atoi(av[1]));
+    return (SUCCESS);
+}
