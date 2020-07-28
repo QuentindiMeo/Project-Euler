6c8a989b598bcd865d13cd34a39da99708a4f111
Quentin <QDM@localhost.localdomain>
2020-07-28 16:59:45 +0200

    [ADD] prob12: adding problem12

---
+++ prob12/main_hdtn.c
@@ -0,0 +1,46 @@
+/*
+** PERSONAL PROJECT, 2019
+** hdtn
+** File description:
+** main hdtn
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "my.h"
+
+static int get_divisors(uint triangle)
+{
+    uint divisors = 0;
+
+    if (triangle < 75000000 || triangle % 4)
+        return (0);
+    for (long i = 1; i <= triangle; i++)
+        if (triangle % i == 0)
+            divisors++;
+    if (divisors >= 350)
+        printf("%i = %d\n", triangle, divisors);
+    return (divisors);
+}
+
+static void hdtn(int limit)
+{
+    uint triangle = 0;
+
+    for (uint i = 1; i; i++) {
+        triangle += i;
+        if (get_divisors(triangle) > limit)
+            break;
+    }
+    printf("1st triangle number to have %d+ divisors: %i\n", limit, triangle);
+}
+
+int main(int ac, char **av)
+{
+    if (ac != 2 || (ac == 2 && atoi(av[1]) < 0)) {
+        printf("USAGE\n\t%s [limit > 0]\n", av[0]);
+        return (FAILURE);
+    }
+    hdtn(atoi(av[1]));
+    return (SUCCESS);
+}
---
+++ prob12/my.h
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2018
+** CPool_Day09_2018
+** File description:
+** my library headers
+*/
+
+#ifndef MY_H_
+#define MY_H_
+
+typedef enum {
+    SUCCESS = 0,
+    FAILURE = 84,
+} return_values_t;
+
+typedef	unsigned int uint;
+
+#endif
